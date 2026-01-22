ad18a96cc33b0097932ff93d76a73f94a8cbb26e
Niko Guo <niko.guo@ehealth.com>
2026-01-22 09:02:55 +0800

    feat: add Google Analytics tracking scripts to the website.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,6 +21,17 @@ export default defineNuxtConfig({
                 { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
                 { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
                 { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap' }
+            ],
+            script: [
+                { src: 'https://www.googletagmanager.com/gtag/js?id=G-M5VNH1F9Z7', async: true },
+                {
+                    innerHTML: `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-M5VNH1F9Z7');
+                    `
+                }
             ]
         }
     }
